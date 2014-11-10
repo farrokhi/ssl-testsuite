450eab035d6e8b5dcf1b36b87856fa94fc2451ee
Babak Farrokhi <babak@farrokhi.net>
2014-11-10 13:42:03 +0330

    - add new test targets - remove SSLv3 checks (obsolete protocol) - add granularity to TLS tests with curl (1.0, 1.1 and 1.2)

--- ssl-test.sh
+++ ssl-test.sh
@@ -2,7 +2,7 @@
 
 # TODO: add preflight check (minimum requirements)
 
-SITE_LIST="www.kismetwireless.net macupdate.com github.com google.com svn.nmap.org news.ycombinator.com www.ripe.net login.yahoo.com bitbucket.org"
+SITE_LIST="www.kismetwireless.net macupdate.com github.com google.com svn.nmap.org news.ycombinator.com www.ripe.net login.yahoo.com bitbucket.org filippo.io"
 FILTER=1
 ENVIRON="no-filter"
 [ $FILTER -gt 0 ] && ENVIRON="filter"
@@ -11,10 +11,12 @@ printf "Running tests in parallel "
 
 for SITE in $SITE_LIST
 do
-	openssl s_client -ssl3 -connect "${SITE}:443" < /dev/null > ${ENVIRON}-ssl3-connect-${SITE}.log 2>&1 && printf "." &
+#	openssl s_client -ssl3 -connect "${SITE}:443" < /dev/null > ${ENVIRON}-ssl3-connect-${SITE}.log 2>&1 && printf "." &
 	openssl s_client -tls1 -connect "${SITE}:443" < /dev/null > ${ENVIRON}-tls1-connect-${SITE}.log 2>&1 && printf "." &
-	curl -3 -o /dev/null -s --trace "${ENVIRON}-ssl3-trace-${SITE}.log" https://${SITE} && printf "." &
-	curl -1 -o /dev/null -s --trace "${ENVIRON}-tls1-trace-${SITE}.log" https://${SITE} && printf "." &
+#	curl -3 -o /dev/null -s --trace "${ENVIRON}-ssl3-trace-${SITE}.log" https://${SITE} && printf "." &
+	curl --tlsv1.0 -o /dev/null -s --trace "${ENVIRON}-tlsv1.0-trace-${SITE}.log" https://${SITE} && printf "." &
+	curl --tlsv1.1 -o /dev/null -s --trace "${ENVIRON}-tlsv1.1-trace-${SITE}.log" https://${SITE} && printf "." &
+	curl --tlsv1.2 -o /dev/null -s --trace "${ENVIRON}-tlsv1.2-trace-${SITE}.log" https://${SITE} && printf "." &
 done
 
 wait
